3580432b8e12b4004e6a661a1d780763365025e1
muiremoraes <muiremoraes@gmail.com>
2025-11-27 20:28:28 +0000

    Try implementing login functionality.

---
+++ test-app/src/Login.jsx
@@ -0,0 +1,10 @@
+function Login(){
+    return <>
+    <h1>Login</h1>
+    Name:&nbsp;<input type="text"></input><br></br>
+    Password:&nbsp;<input type="password"></input>
+    <br></br>
+    <button>Log in</button>
+    </>
+}
+export default Login;
